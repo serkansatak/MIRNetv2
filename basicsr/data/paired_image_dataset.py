0fb2d3f5b616f32f314f3d3459e2accd918aa5e2
serkansatak <serkansatak1@gmail.com>
2023-06-26 20:05:55 +0300

    corrections on dataset_config

--- basicsr/data/paired_image_dataset.py
+++ basicsr/data/paired_image_dataset.py
@@ -59,8 +59,8 @@ class Dataset_PairedImage(data.Dataset):
             self.filename_tmpl = '{}'
 
         # temporal augmentation
-        if (self.io_backend_opt['type'] == 'disk') and ('date_file_csv' in self.io_backend_opt.keys()):
-            self.paths = paired_paths_from_csv(self.io_backend_opt['data_list_file'], ['lq','gt'])
+        if (self.io_backend_opt['type'] == 'disk') and ('data_list_csv' in self.io_backend_opt.keys()):
+            self.paths = paired_paths_from_csv(self.io_backend_opt['data_list_csv'], ['lq','gt'])
         
         elif self.io_backend_opt['type'] == 'lmdb':
             self.io_backend_opt['db_paths'] = [self.lq_folder, self.gt_folder]
